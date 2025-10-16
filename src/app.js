4a462a900df67c288999c4cd9586d3373518ec54
Mohamed Sikkander I <sikkander1998@gmail.com>
2025-10-16 17:19:56 +0530

    implemented feed and user routes

--- src/app.js
+++ src/app.js
@@ -2,22 +2,68 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/database');
 const User = require('./models/user');
+app.use(express.json());
 
 app.post('/signup', async (req, res) => {
-    const userObj = {
-        firstName: "Suriya",
-        lastName: "Kala",
-        emailId: "suriya.kala@gmail.com",
-        password: "abcd1234",
-        age: 26,
-        gender: "Female"
-    }
+    console.log(req.body);
     try {
-        const newUser = new User(userObj);
+        const newUser = new User(req.body);
         await newUser.save();
         res.send("User created successfully!");
     } catch (err) {
         res.send("Error in creating user: ", err.message);
+    }
+})
+
+app.get('/user', async (req, res) => {
+    try {
+        const user = await User.findOne({ emailId: req.body.emailId });
+        if(!user) {
+            return res.status(404).send("User not found");
+        }else {
+            res.json(user);
+        }
+    } catch (err) {
+        res.send("Something went wrong: ", err.message);
+    }
+})
+
+app.get('/feed', async (req, res) => {
+    try {
+        const users = await User.find({});
+        if(users.length === 0) {
+            return res.status(404).send("No users found");
+        }else {
+            res.json(users);
+        }
+    } catch (err) {
+        res.send("Something went wrong: ", err.message);
+    }
+})
+
+app.delete('/user', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.body.userId);
+        if(!user) {
+            return res.status(404).send("User not found");
+        }else {
+            res.send("User deleted successfully");
+        }
+    } catch (err) {
+        res.send("Something went wrong: ", err.message);
+    }
+})
+
+app.patch('/user', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.body.userId, req.body)
+        if(!user) {
+            return res.status(404).send("User not found");
+        }else {
+            res.send("User updated successfully");
+        }
+    } catch (err) {
+        res.status(404).send("Something went wrong: ", err.message);
     }
 })
 
