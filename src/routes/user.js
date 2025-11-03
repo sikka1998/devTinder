ba26ce8faa978f7ddc866344287fbdc7cc8fab61
Mohamed Sikkander I <sikkander1998@gmail.com>
2025-11-03 15:27:34 +0530

    Refactor connection request schema and enhance user routes for fetching connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const userRouter = express.Router();
 const { authUser } = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
 
+const USER_SAFE_DATA = 'firstName lastName age skills photoURL';
+
 userRouter.get('/requests/received', authUser, async (req, res) => {
     try {
         const loggedInUser = req.user;
@@ -10,7 +12,7 @@ userRouter.get('/requests/received', authUser, async (req, res) => {
         const connectRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status: 'interested'
-        }).populate('fromUserId', 'firstName lastName age skills photoURL')
+        }).populate('fromUserId', USER_SAFE_DATA)
 
         res.json({
             message: "Connection Requests Fetched Successfully",
@@ -19,6 +21,34 @@ userRouter.get('/requests/received', authUser, async (req, res) => {
     } catch(err) {
         res.status(400).send("Error while fetching received requests: " + err.message);
     } 
+})
+
+userRouter.get('/connections', authUser, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id, status: 'accepted' },
+                { toUserId: loggedInUser._id, status: 'accepted' }
+            ]
+        }).populate('fromUserId toUserId', USER_SAFE_DATA);
+
+        const data = connectionRequests.map( request => {
+            if(request.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return request.toUserId;
+            }
+
+            return request.fromUserId;
+        })
+
+        res.json({
+            message: "Connections Fetched Successfully",
+            data: data
+        })
+    } catch(err) {
+        res.status(400).send("Error while fetching connections: " + err.message);
+    }
 })
 
 module.exports = userRouter;
